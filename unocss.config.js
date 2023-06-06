9bec1550a28a779fd1730f3a75891310c5a9b1a7
zhongxingdou <zhongxingdou@ledayun.com>
2023-06-06 18:12:48 +0800

    fix eslint error

--- unocss.config.js
+++ unocss.config.js
@@ -1,7 +1,8 @@
-import { defineConfig, presetUno } from "unocss"
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig, presetUno } from 'unocss'
 import presetIcons from '@unocss/preset-icons'
 
-const remRE = /^-?[\.\d]+rem$/
+const remRE = /^-?[.\d]+rem$/
 
 export default defineConfig(
   {
@@ -9,22 +10,24 @@ export default defineConfig(
       presetIcons({
         extraProperties: {
           display: 'inline-block',
-          'vertical-align': 'middle'
-        }
+          'vertical-align': 'middle',
+        },
       }),
       presetUno(),
     ],
-    theme:{
+    theme: {
       // 解决小程序不支持 * 选择器
-      preflightRoot: ["page,::before,::after"]
+      preflightRoot: ['page,::before,::after'],
     },
     postprocess(util) {
       // 自定义rem 转 rpx
       util.entries.forEach((i) => {
         const value = i[1]
-        if (value && typeof value === 'string' && remRE.test(value))
+        if (value && typeof value === 'string' && remRE.test(value)) {
+          // eslint-disable-next-line no-param-reassign
           i[1] = `${value.slice(0, -3) * 16 * 2}rpx`
+        }
       })
     },
-  }
+  },
 )
